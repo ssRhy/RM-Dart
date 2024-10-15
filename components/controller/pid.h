b57ef02a63af30404be02e7b50ce76561eda7885
是小企鹅呀 <1357482552@qq.com>
2024-10-15 22:59:26 +0800

    ✨ feat(pid): 尝试改良PID？

--- components/controller/pid.h
+++ components/controller/pid.h
@@ -17,11 +17,7 @@
 #ifndef PID_H
 #define PID_H
 #include "struct_typedef.h"
-enum PID_MODE
-{
-    PID_POSITION = 0,
-    PID_DELTA
-};
+enum PID_MODE { PID_POSITION = 0, PID_DELTA };
 
 typedef struct
 {
@@ -31,8 +27,8 @@ typedef struct
     fp32 Ki;
     fp32 Kd;
 
-    fp32 max_out;  //最大输出
-    fp32 max_iout; //最大积分输出
+    fp32 max_out;   //最大输出
+    fp32 max_iout;  //最大积分输出
 
     fp32 set;
     fp32 fdb;
@@ -41,10 +37,32 @@ typedef struct
     fp32 Pout;
     fp32 Iout;
     fp32 Dout;
-    fp32 Dbuf[3];  //微分项 0最新 1上一次 2上上次
-    fp32 error[3]; //误差项 0最新 1上一次 2上上次
+    fp32 Dbuf[3];   //微分项 0最新 1上一次 2上上次
+    fp32 error[3];  //误差项 0最新 1上一次 2上上次
 
 } pid_type_def;
+
+typedef struct Pid_t
+{
+    // PID 参数
+    fp32 Kp;
+    fp32 Ki;
+    fp32 Kd;
+    fp32 N;  // 滤波器系数
+
+    // 饱和限制
+    fp32 max_out;   //最大输出
+    fp32 max_iout;  //最大积分输出
+
+    fp32 ref;  // 设定值
+    fp32 fdb;  // 反馈值
+
+    // PID 输出
+    fp32 out;
+    fp32 Pout;
+    fp32 Iout;
+    fp32 Dout;
+} Pid_t;
 
 /**
   * @brief          pid struct data init
@@ -56,7 +74,8 @@ typedef struct
   * @param[in]      max_iout: pid最大积分输出
   * @retval         none
   */
-extern void PID_init(pid_type_def *pid, uint8_t mode, const fp32 PID[3], fp32 max_out, fp32 max_iout);
+extern void PID_init(
+    pid_type_def * pid, uint8_t mode, const fp32 PID[3], fp32 max_out, fp32 max_iout);
 
 /**
   * @brief          pid计算
@@ -65,13 +84,14 @@ extern void PID_init(pid_type_def *pid, uint8_t mode, const fp32 PID[3], fp32 ma
   * @param[in]      set: 设定值
   * @retval         pid输出
   */
-extern fp32 PID_calc(pid_type_def *pid, fp32 ref, fp32 set);
+extern fp32 PID_calc(pid_type_def * pid, fp32 ref, fp32 set);
 
 /**
   * @brief          pid 输出清除
   * @param[out]     pid: PID结构数据指针
   * @retval         none
   */
-extern void PID_clear(pid_type_def *pid);
+extern void PID_clear(pid_type_def * pid);
 
 #endif
+/*------------------------------ End of File ------------------------------*/
