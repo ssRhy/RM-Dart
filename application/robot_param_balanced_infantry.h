e263d09b111332d559b291fe13d853b2663bc7a6
是小企鹅呀 <1357482552@qq.com>
2024-10-20 23:47:48 +0800

    这个参数感觉也不错

--- application/robot_param_balanced_infantry.h
+++ application/robot_param_balanced_infantry.h
@@ -226,7 +226,7 @@
     // 腿长跟踪长度环PID参数
     #define KP_CHASSIS_LEG_LENGTH_LENGTH        (70.0f)
     #define KI_CHASSIS_LEG_LENGTH_LENGTH        (0.0f)
-    #define KD_CHASSIS_LEG_LENGTH_LENGTH        (3.0f)
+    #define KD_CHASSIS_LEG_LENGTH_LENGTH        (15.0f)
     #define MAX_IOUT_CHASSIS_LEG_LENGTH_LENGTH  (1.0f)
     #define MAX_OUT_CHASSIS_LEG_LENGTH_LENGTH   (30.0f)
 
