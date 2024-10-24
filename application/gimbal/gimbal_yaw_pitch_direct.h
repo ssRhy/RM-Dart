61cc4267b8f1ee1a6f6d9820a24b8ad6fa8e04c2
Harry_Wang <1120230643@smbu.edu.cn>
2024-10-24 22:52:43 +0800

    ✨ feat(加入了基于电机角度值的启动yaw轴校准模式):

--- application/gimbal/gimbal_yaw_pitch_direct.h
+++ application/gimbal/gimbal_yaw_pitch_direct.h
@@ -38,8 +38,8 @@
  */
 typedef enum {
     GIMBAL_ZERO_FORCE,  // 云台无力，所有控制量置0
-    GIMBAL_GYRO,        // 云台陀螺仪控制
-    GIMBAL_ANGLE,        // 遥控器的值乘以比例成电流值开环控制
+    GIMBAL_IMU,        // 云台陀螺仪控制
+    GIMBAL_INIT,
 } GimbalMode_e;
 
 
