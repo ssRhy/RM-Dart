e4ac3688c0939cbe610facae9ebd98389c47b637
是小企鹅呀 <1357482552@qq.com>
2024-11-21 21:00:12 +0800

    更新macro_typedef

--- application/typedef/macro_typedef.h
+++ application/typedef/macro_typedef.h
@@ -1,10 +1,13 @@
+#ifndef MACRO_TYPEDEF_H
+#define MACRO_TYPEDEF_H
 
-#define rc_deadband_limit(input, output, dealine)          \
-    {                                                      \
-        if ((input) > (dealine) || (input) < -(dealine)) { \
-            (output) = (input);                            \
-        } else {                                           \
-            (output) = 0;                                  \
-        }                                                  \
-    }
+#define AX_X 0
+#define AX_Y 1
+#define AX_Z 2
+
+#define AX_ROLL AX_X
+#define AX_PITCH AX_Y
+#define AX_YAW AX_Z
+
+#endif  // MACRO_TYPEDEF_H
 /*------------------------------ End of File ------------------------------*/
