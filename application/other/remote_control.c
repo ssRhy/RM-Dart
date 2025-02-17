0353d983ce7f7f470ebdc9c88fbce738c79824ca
是小企鹅呀 <1357482552@qq.com>
2025-02-17 22:44:27 +0800

    📃 docs(remote_control.c): 添加注释

--- application/other/remote_control.c
+++ application/other/remote_control.c
@@ -1,5 +1,5 @@
 /**
-  ****************************(C) COPYRIGHT 2019 DJI****************************
+  ****************************(C) COPYRIGHT 2025 Polarbear****************************
   * @file       remote_control.c/h
   * @brief      遥控器处理，遥控器是通过类似SBUS的协议传输，利用DMA传输方式节约CPU
   *             资源，利用串口空闲中断来拉起处理函数，同时提供一些掉线重启DMA，串口
@@ -9,15 +9,20 @@
   *  Version    Date            Author          Modification
   *  V1.0.0     Dec-26-2018     RM              1. done
   *  V1.0.0     Nov-11-2019     RM              1. support development board tpye c
+  *  V2.0.0     Feb-17-2025     Penguin         1. support RC AT9S PRO
+  *                                             2. support RC HT8A
+  *                                             3. support normal sbus RC in struct Sbus_t
   *
   @verbatim
   ==============================================================================
   使用At9sPro遥控器时请设置5通为SwE，6通为SwG
 
   注：使用非DT7遥控器时，需要先检查通道值数据是否正常（一般遥控器都带有通道值数据偏移功能，将通道值中值移动到正确数值后再使用）
+      AT9S PRO 遥控器中值为 1000
+      HT8A 遥控器中值为 992
   ==============================================================================
   @endverbatim
-  ****************************(C) COPYRIGHT 2019 DJI****************************
+  ****************************(C) COPYRIGHT 2025 Polarbear****************************
   */
 
 // clang-format off
@@ -200,6 +205,7 @@ void USART3_IRQHandler(void)
 
             if(this_time_rx_len == RC_FRAME_LENGTH)
             {
+                //处理遥控器数据
                 sbus_to_rc(sbus_rx_buf[0], &rc_ctrl);
                 //记录数据接收时间
                 detect_hook(DBUS_TOE);
@@ -207,6 +213,7 @@ void USART3_IRQHandler(void)
             } 
             else if (this_time_rx_len == SBUS_RC_FRAME_LENGTH)
             {
+                //处理遥控器数据
 #if (__RC_TYPE == RC_AT9S_PRO)
                 At9sProSbusToRc(sbus_rx_buf[0], &rc_ctrl);
 #elif (__RC_TYPE == RC_HT8A)
@@ -249,6 +256,7 @@ void USART3_IRQHandler(void)
             }
             else if (this_time_rx_len == SBUS_RC_FRAME_LENGTH)
             {
+                //处理遥控器数据
 #if (__RC_TYPE == RC_AT9S_PRO)
                 At9sProSbusToRc(sbus_rx_buf[1], &rc_ctrl);
 #elif (__RC_TYPE == RC_HT8A)
