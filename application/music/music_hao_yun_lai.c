dad4784ac25386b9e3b136efb0896af7feb5116b
是小企鹅呀 <1357482552@qq.com>
2025-04-04 23:54:11 +0800

    🎈 perf(好运来): 优化编写，减少音符个数

--- application/music/music_hao_yun_lai.c
+++ application/music/music_hao_yun_lai.c
@@ -20,7 +20,7 @@
 #define ONE_FOURTH_HALF 125
 #define ONE_FOURTH_HALF_HALF 63
 
-#define NOTE_NUM 175
+#define NOTE_NUM 140
 static Note Notes[NOTE_NUM];  // Array of notes
 static MusicInfo_s MUSIC_INFO;
 
@@ -35,12 +35,11 @@ MusicInfo_s MusicHaoYunLaiInit(void)
 
     // 好运来祝你
     // 6 3`_ 2`_ 2` 1`_ 6_
-    WRITE_NOTE(D_la, ONE_FOURTH);           // 6
-    WRITE_NOTE(D_mi * 2, ONE_FOURTH_HALF);  // 3`_
-    WRITE_NOTE(D_re * 2, ONE_FOURTH_HALF);  // 2`_
-    WRITE_NOTE(D_re * 2, ONE_FOURTH);       // 2`
-    WRITE_NOTE(D_do * 2, ONE_FOURTH_HALF);  // 1`_
-    WRITE_NOTE(D_la, ONE_FOURTH_HALF);      // 6_
+    WRITE_NOTE(D_la, ONE_FOURTH);                        // 6
+    WRITE_NOTE(D_mi * 2, ONE_FOURTH_HALF);               // 3`_
+    WRITE_NOTE(D_re * 2, ONE_FOURTH_HALF + ONE_FOURTH);  // 2`_ 2`
+    WRITE_NOTE(D_do * 2, ONE_FOURTH_HALF);               // 1`_
+    WRITE_NOTE(D_la, ONE_FOURTH_HALF);                   // 6_
 
     // 好运来，
     // 5 1`_ 2`_ 6 -
@@ -66,12 +65,11 @@ MusicInfo_s MusicHaoYunLaiInit(void)
 
     // 好运来我们
     // 3 6_ 5_ 6 6_ 5_
-    WRITE_NOTE(D_mi, ONE_FOURTH);       // 3
-    WRITE_NOTE(D_la, ONE_FOURTH_HALF);  // 6_
-    WRITE_NOTE(D_so, ONE_FOURTH_HALF);  // 5_
-    WRITE_NOTE(D_la, ONE_FOURTH);       // 6
-    WRITE_NOTE(D_la, ONE_FOURTH_HALF);  // 6_
-    WRITE_NOTE(D_so, ONE_FOURTH_HALF);  // 5_
+    WRITE_NOTE(D_mi, ONE_FOURTH);                    // 3
+    WRITE_NOTE(D_la, ONE_FOURTH_HALF);               // 6_
+    WRITE_NOTE(D_so, ONE_FOURTH_HALF);               // 5_
+    WRITE_NOTE(D_la, ONE_FOURTH + ONE_FOURTH_HALF);  // 6 6_
+    WRITE_NOTE(D_so, ONE_FOURTH_HALF);               // 5_
 
     // 好运来，
     // 6 2`_ 1`_ 2` -
@@ -82,75 +80,62 @@ MusicInfo_s MusicHaoYunLaiInit(void)
 
     //迎着好运兴旺发达
     // 1`-_ 1`__ 1`_ 2`_ 3`_ 3`_ 2`_ 1`_
-    WRITE_NOTE(D_do * 2, ONE_FOURTH_HALF + ONE_FOURTH_HALF_HALF);  // 1`-_
-    WRITE_NOTE(D_do * 2, ONE_FOURTH_HALF_HALF);                    // 1`__
-    WRITE_NOTE(D_do * 2, ONE_FOURTH_HALF);                         // 1`_
-    WRITE_NOTE(D_re * 2, ONE_FOURTH_HALF);                         // 2`_
-    WRITE_NOTE(D_mi * 2, ONE_FOURTH_HALF);                         // 3`_
-    WRITE_NOTE(D_mi * 2, ONE_FOURTH_HALF);                         // 3`_
-    WRITE_NOTE(D_re * 2, ONE_FOURTH_HALF);                         // 2`_
-    WRITE_NOTE(D_do * 2, ONE_FOURTH_HALF);                         // 1`_
+    WRITE_NOTE(
+        D_do * 2, ONE_FOURTH_HALF + ONE_FOURTH_HALF_HALF + ONE_FOURTH_HALF_HALF +
+                      ONE_FOURTH_HALF);                       // 1`-_ 1`__ 1`_
+    WRITE_NOTE(D_re * 2, ONE_FOURTH_HALF);                    // 2`_
+    WRITE_NOTE(D_mi * 2, ONE_FOURTH_HALF + ONE_FOURTH_HALF);  // 3`_ 3`_
+    WRITE_NOTE(D_re * 2, ONE_FOURTH_HALF);                    // 2`_
+    WRITE_NOTE(D_do * 2, ONE_FOURTH_HALF);                    // 1`_
 
     // 通四海。
     // 5 1`_ 6_ 6 -
-    WRITE_NOTE(D_so, ONE_FOURTH);           // 5
-    WRITE_NOTE(D_do * 2, ONE_FOURTH_HALF);  // 1`_
-    WRITE_NOTE(D_la, ONE_FOURTH_HALF);      // 6_
-    WRITE_NOTE(D_la, ONE_FOURTH * 2);       // 6
+    WRITE_NOTE(D_so, ONE_FOURTH);                        // 5
+    WRITE_NOTE(D_do * 2, ONE_FOURTH_HALF);               // 1`_
+    WRITE_NOTE(D_la, ONE_FOURTH_HALF + ONE_FOURTH * 2);  // 6_ 6
 
     // 尾奏
     // 6 - - -
     WRITE_NOTE(D_la, ONE_FOURTH * 4);  // 6
 
     //6__ 5__ 3_ 2_ 2_ 2__ 1__ 2__ 3__ 5_ 5_
-    WRITE_NOTE(D_la, ONE_FOURTH_HALF_HALF);  // 6__
-    WRITE_NOTE(D_so, ONE_FOURTH_HALF_HALF);  // 5__
-    WRITE_NOTE(D_mi, ONE_FOURTH_HALF);       // 3_
-    WRITE_NOTE(D_re, ONE_FOURTH_HALF);       // 2_
-    WRITE_NOTE(D_re, ONE_FOURTH_HALF);       // 2_
-    WRITE_NOTE(D_re, ONE_FOURTH_HALF_HALF);  // 2__
-    WRITE_NOTE(D_do, ONE_FOURTH_HALF_HALF);  // 1__
-    WRITE_NOTE(D_re, ONE_FOURTH_HALF_HALF);  // 2__
-    WRITE_NOTE(D_mi, ONE_FOURTH_HALF_HALF);  // 3__
-    WRITE_NOTE(D_so, ONE_FOURTH_HALF);       // 5_
-    WRITE_NOTE(D_so, ONE_FOURTH_HALF);       // 5_
+    WRITE_NOTE(D_la, ONE_FOURTH_HALF_HALF);                                      // 6__
+    WRITE_NOTE(D_so, ONE_FOURTH_HALF_HALF);                                      // 5__
+    WRITE_NOTE(D_mi, ONE_FOURTH_HALF);                                           // 3_
+    WRITE_NOTE(D_re, ONE_FOURTH_HALF + ONE_FOURTH_HALF + ONE_FOURTH_HALF_HALF);  // 2_ 2_ 2__
+    WRITE_NOTE(D_do, ONE_FOURTH_HALF_HALF);                                      // 1__
+    WRITE_NOTE(D_re, ONE_FOURTH_HALF_HALF);                                      // 2__
+    WRITE_NOTE(D_mi, ONE_FOURTH_HALF_HALF);                                      // 3__
+    WRITE_NOTE(D_so, ONE_FOURTH_HALF + ONE_FOURTH_HALF);                         // 5_ 5_
 
     // 5__ 2__ 3__ 5__ 6_ 6_ 6__ 3__ 5__ 6__ 1`_ 1`_
-    WRITE_NOTE(D_so, ONE_FOURTH_HALF_HALF);  // 5__
-    WRITE_NOTE(D_re, ONE_FOURTH_HALF_HALF);  // 2__
-    WRITE_NOTE(D_mi, ONE_FOURTH_HALF);       // 3__
-    WRITE_NOTE(D_so, ONE_FOURTH_HALF);       // 5__
-    WRITE_NOTE(D_la, ONE_FOURTH_HALF);       // 6_
-    WRITE_NOTE(D_la, ONE_FOURTH_HALF);       // 6_
-    WRITE_NOTE(D_la, ONE_FOURTH_HALF_HALF);  // 6__
-    WRITE_NOTE(D_mi, ONE_FOURTH_HALF);       // 3__
-    WRITE_NOTE(D_so, ONE_FOURTH_HALF);       // 5__
-    WRITE_NOTE(D_la, ONE_FOURTH_HALF);       // 6__
-    WRITE_NOTE(D_do * 2, ONE_FOURTH_HALF);   // 1`_
-    WRITE_NOTE(D_do * 2, ONE_FOURTH_HALF);   // 1`_
+    WRITE_NOTE(D_so, ONE_FOURTH_HALF_HALF);                                      // 5__
+    WRITE_NOTE(D_re, ONE_FOURTH_HALF_HALF);                                      // 2__
+    WRITE_NOTE(D_mi, ONE_FOURTH_HALF);                                           // 3__
+    WRITE_NOTE(D_so, ONE_FOURTH_HALF);                                           // 5__
+    WRITE_NOTE(D_la, ONE_FOURTH_HALF + ONE_FOURTH_HALF + ONE_FOURTH_HALF_HALF);  // 6_ 6_ 6__
+    WRITE_NOTE(D_mi, ONE_FOURTH_HALF);                                           // 3__
+    WRITE_NOTE(D_so, ONE_FOURTH_HALF);                                           // 5__
+    WRITE_NOTE(D_la, ONE_FOURTH_HALF);                                           // 6__
+    WRITE_NOTE(D_do * 2, ONE_FOURTH_HALF + ONE_FOURTH_HALF);                     // 1`_ 1`_
 
     // 1`__ 6__ 1`__ 2`__ 3`_ 3`_ 0 3`_ 3`_
-    WRITE_NOTE(D_do * 2, ONE_FOURTH_HALF_HALF);  // 1`__
-    WRITE_NOTE(D_la, ONE_FOURTH_HALF_HALF);      // 6__
-    WRITE_NOTE(D_do * 2, ONE_FOURTH_HALF_HALF);  // 1`__
-    WRITE_NOTE(D_re * 2, ONE_FOURTH_HALF_HALF);  // 2`__
-    WRITE_NOTE(D_mi * 2, ONE_FOURTH_HALF);       // 3`_
-    WRITE_NOTE(D_mi * 2, ONE_FOURTH_HALF);       // 3`_
-    WRITE_NOTE(0, ONE_FOURTH_HALF);              // 0
-    WRITE_NOTE(D_mi * 2, ONE_FOURTH_HALF);       // 3`_
-    WRITE_NOTE(D_mi * 2, ONE_FOURTH_HALF);       // 3`_
+    WRITE_NOTE(D_do * 2, ONE_FOURTH_HALF_HALF);               // 1`__
+    WRITE_NOTE(D_la, ONE_FOURTH_HALF_HALF);                   // 6__
+    WRITE_NOTE(D_do * 2, ONE_FOURTH_HALF_HALF);               // 1`__
+    WRITE_NOTE(D_re * 2, ONE_FOURTH_HALF_HALF);               // 2`__
+    WRITE_NOTE(D_mi * 2, ONE_FOURTH_HALF + ONE_FOURTH_HALF);  // 3`_ 3`_
+    WRITE_NOTE(0, ONE_FOURTH_HALF);                           // 0
+    WRITE_NOTE(D_mi * 2, ONE_FOURTH_HALF + ONE_FOURTH_HALF);  // 3`_ 3`_
 
     // 停顿一下
     WRITE_NOTE(0, ONE_FOURTH_HALF_HALF);
 
     // 叠个千纸鹤 再
     // 6-_ 6__ 1`_ 1`_ 6- 6_
-    WRITE_NOTE(D_la, ONE_FOURTH_HALF + ONE_FOURTH_HALF_HALF);  // 6-_
-    WRITE_NOTE(D_la, ONE_FOURTH_HALF_HALF);                    // 6__
-    WRITE_NOTE(D_do * 2, ONE_FOURTH_HALF);                     // 1`_
-    WRITE_NOTE(D_do * 2, ONE_FOURTH_HALF);                     // 1`_
-    WRITE_NOTE(D_la, ONE_FOURTH + ONE_FOURTH_HALF);            // 6-
-    WRITE_NOTE(D_la, ONE_FOURTH_HALF);                         // 6_
+    WRITE_NOTE(D_la, ONE_FOURTH_HALF + ONE_FOURTH_HALF_HALF + ONE_FOURTH_HALF_HALF);  // 6-_ 6__
+    WRITE_NOTE(D_do * 2, ONE_FOURTH_HALF + ONE_FOURTH_HALF);                          // 1`_ 1`_
+    WRITE_NOTE(D_la, ONE_FOURTH + ONE_FOURTH_HALF + ONE_FOURTH_HALF);                 // 6- 6_
 
     // 系个红飘带，
     // 5_ 3_ 5_ 1`_ 6 -
@@ -162,22 +147,20 @@ MusicInfo_s MusicHaoYunLaiInit(void)
 
     // 愿善良的人们
     // 6_ 1`_ 1`-_ 1`__ 1`_ 6_ 5
-    WRITE_NOTE(D_la, ONE_FOURTH_HALF);                   // 6_
-    WRITE_NOTE(D_do * 2, ONE_FOURTH_HALF);               // 1`_
-    WRITE_NOTE(D_do * 2, ONE_FOURTH + ONE_FOURTH_HALF);  // 1`-_
-    WRITE_NOTE(D_do * 2, ONE_FOURTH_HALF_HALF);          // 1`__
-    WRITE_NOTE(D_do * 2, ONE_FOURTH_HALF);               // 1`_
-    WRITE_NOTE(D_la, ONE_FOURTH_HALF);                   // 6_
-    WRITE_NOTE(D_so, ONE_FOURTH);                        // 5
+    WRITE_NOTE(D_la, ONE_FOURTH_HALF);  // 6_
+    WRITE_NOTE(
+        D_do * 2, ONE_FOURTH_HALF + ONE_FOURTH + ONE_FOURTH_HALF + ONE_FOURTH_HALF_HALF +
+                      ONE_FOURTH_HALF);  // 1`_ 1`-_ 1`__ 1`_
+    WRITE_NOTE(D_la, ONE_FOURTH_HALF);   // 6_
+    WRITE_NOTE(D_so, ONE_FOURTH);        // 5
 
     // 天天好运来，你
     // 6_ 5_ 2_ 5_ 3- 3_
-    WRITE_NOTE(D_la, ONE_FOURTH_HALF);               // 6_
-    WRITE_NOTE(D_so, ONE_FOURTH_HALF);               // 5_
-    WRITE_NOTE(D_re, ONE_FOURTH_HALF);               // 2_
-    WRITE_NOTE(D_so, ONE_FOURTH_HALF);               // 5_
-    WRITE_NOTE(D_mi, ONE_FOURTH + ONE_FOURTH_HALF);  // 3-
-    WRITE_NOTE(D_mi, ONE_FOURTH_HALF);               // 3_
+    WRITE_NOTE(D_la, ONE_FOURTH_HALF);                                 // 6_
+    WRITE_NOTE(D_so, ONE_FOURTH_HALF);                                 // 5_
+    WRITE_NOTE(D_re, ONE_FOURTH_HALF);                                 // 2_
+    WRITE_NOTE(D_so, ONE_FOURTH_HALF);                                 // 5_
+    WRITE_NOTE(D_mi, ONE_FOURTH + ONE_FOURTH_HALF + ONE_FOURTH_HALF);  // 3- 3_
 
     // 勤劳生活美 你
     // 3_ 2_ 1_ 3_ 2- 3_
@@ -198,14 +181,11 @@ MusicInfo_s MusicHaoYunLaiInit(void)
 
     // 你一生的忙碌为了
     // 6_ 1`_ 1`-_ 6__ 2`_ 2`_ 2`_ 1`_
-    WRITE_NOTE(D_la, ONE_FOURTH_HALF);                             // 6_
-    WRITE_NOTE(D_do * 2, ONE_FOURTH_HALF);                         // 1`_
-    WRITE_NOTE(D_do * 2, ONE_FOURTH_HALF + ONE_FOURTH_HALF_HALF);  // 1`-_
-    WRITE_NOTE(D_la, ONE_FOURTH_HALF_HALF);                        // 6__
-    WRITE_NOTE(D_re * 2, ONE_FOURTH_HALF);                         // 2`_
-    WRITE_NOTE(D_re * 2, ONE_FOURTH_HALF);                         // 2`_
-    WRITE_NOTE(D_re * 2, ONE_FOURTH_HALF);                         // 2`_
-    WRITE_NOTE(D_do * 2, ONE_FOURTH_HALF);                         // 1`_
+    WRITE_NOTE(D_la, ONE_FOURTH_HALF);                                               // 6_
+    WRITE_NOTE(D_do * 2, ONE_FOURTH_HALF + ONE_FOURTH_HALF + ONE_FOURTH_HALF_HALF);  // 1`_ 1`-_
+    WRITE_NOTE(D_la, ONE_FOURTH_HALF_HALF);                                          // 6__
+    WRITE_NOTE(D_re * 2, ONE_FOURTH_HALF + ONE_FOURTH_HALF + ONE_FOURTH_HALF);       // 2`_ 2`_ 2`_
+    WRITE_NOTE(D_do * 2, ONE_FOURTH_HALF);                                           // 1`_
 
     // 笑逐颜
     // 2/4 6 5_ 1`_
@@ -222,12 +202,9 @@ MusicInfo_s MusicHaoYunLaiInit(void)
 
     // 打个中国结 请
     // 6-_ 6__ 1`_ 1`_ 6- 6_
-    WRITE_NOTE(D_la, ONE_FOURTH_HALF + ONE_FOURTH_HALF_HALF);  // 6-_
-    WRITE_NOTE(D_la, ONE_FOURTH_HALF_HALF);                    // 6__
-    WRITE_NOTE(D_do * 2, ONE_FOURTH_HALF);                     // 1`_
-    WRITE_NOTE(D_do * 2, ONE_FOURTH_HALF);                     // 1`_
-    WRITE_NOTE(D_la, ONE_FOURTH + ONE_FOURTH_HALF);            // 6-
-    WRITE_NOTE(D_la, ONE_FOURTH_HALF);                         // 6_
+    WRITE_NOTE(D_la, ONE_FOURTH_HALF + ONE_FOURTH_HALF_HALF + ONE_FOURTH_HALF_HALF);  // 6-_ 6__
+    WRITE_NOTE(D_do * 2, ONE_FOURTH_HALF + ONE_FOURTH_HALF);                          // 1`_ 1`_
+    WRITE_NOTE(D_la, ONE_FOURTH + ONE_FOURTH_HALF + ONE_FOURTH_HALF);                 // 6- 6_
 
     // 春风剪个彩，
     // 5_ 3_ 5_ 1`_ 6 -
@@ -239,22 +216,20 @@ MusicInfo_s MusicHaoYunLaiInit(void)
 
     // 愿祖国的日月
     // 6_ 1`_ 1`-_ 1`__ 1`_ 6_ 5
-    WRITE_NOTE(D_la, ONE_FOURTH_HALF);                   // 6_
-    WRITE_NOTE(D_do * 2, ONE_FOURTH_HALF);               // 1`_
-    WRITE_NOTE(D_do * 2, ONE_FOURTH + ONE_FOURTH_HALF);  // 1`-_
-    WRITE_NOTE(D_do * 2, ONE_FOURTH_HALF_HALF);          // 1`__
-    WRITE_NOTE(D_do * 2, ONE_FOURTH_HALF);               // 1`_
-    WRITE_NOTE(D_la, ONE_FOURTH_HALF);                   // 6_
-    WRITE_NOTE(D_so, ONE_FOURTH);                        // 5
+    WRITE_NOTE(D_la, ONE_FOURTH_HALF);  // 6_
+    WRITE_NOTE(
+        D_do * 2, ONE_FOURTH_HALF + ONE_FOURTH + ONE_FOURTH_HALF + ONE_FOURTH_HALF_HALF +
+                      ONE_FOURTH_HALF);  // 1`_ 1`-_ 1`__ 1`_
+    WRITE_NOTE(D_la, ONE_FOURTH_HALF);   // 6_
+    WRITE_NOTE(D_so, ONE_FOURTH);        // 5
 
     // 年年好运来，你
     // 6_ 5_ 2_ 5_ 3- 3_
-    WRITE_NOTE(D_la, ONE_FOURTH_HALF);               // 6_
-    WRITE_NOTE(D_so, ONE_FOURTH_HALF);               // 5_
-    WRITE_NOTE(D_re, ONE_FOURTH_HALF);               // 2_
-    WRITE_NOTE(D_so, ONE_FOURTH_HALF);               // 5_
-    WRITE_NOTE(D_mi, ONE_FOURTH + ONE_FOURTH_HALF);  // 3-
-    WRITE_NOTE(D_mi, ONE_FOURTH_HALF);               // 3_
+    WRITE_NOTE(D_la, ONE_FOURTH_HALF);                                 // 6_
+    WRITE_NOTE(D_so, ONE_FOURTH_HALF);                                 // 5_
+    WRITE_NOTE(D_re, ONE_FOURTH_HALF);                                 // 2_
+    WRITE_NOTE(D_so, ONE_FOURTH_HALF);                                 // 5_
+    WRITE_NOTE(D_mi, ONE_FOURTH + ONE_FOURTH_HALF + ONE_FOURTH_HALF);  // 3- 3_
 
     // 凤舞太平年 你
     // 3_ 2_ 1_ 3_ 2- 3_
@@ -275,14 +250,11 @@ MusicInfo_s MusicHaoYunLaiInit(void)
 
     // 幸福的家园迎来
     // 6_ 1`_ 1`-_ 6__ 2`_ 2`_ 2`_ 1`_
-    WRITE_NOTE(D_la, ONE_FOURTH_HALF);                             // 6_
-    WRITE_NOTE(D_do * 2, ONE_FOURTH_HALF);                         // 1`_
-    WRITE_NOTE(D_do * 2, ONE_FOURTH_HALF + ONE_FOURTH_HALF_HALF);  // 1`-_
-    WRITE_NOTE(D_la, ONE_FOURTH_HALF_HALF);                        // 6__
-    WRITE_NOTE(D_re * 2, ONE_FOURTH_HALF);                         // 2`_
-    WRITE_NOTE(D_re * 2, ONE_FOURTH_HALF);                         // 2`_
-    WRITE_NOTE(D_re * 2, ONE_FOURTH_HALF);                         // 2`_
-    WRITE_NOTE(D_do * 2, ONE_FOURTH_HALF);                         // 1`_
+    WRITE_NOTE(D_la, ONE_FOURTH_HALF);                                               // 6_
+    WRITE_NOTE(D_do * 2, ONE_FOURTH_HALF + ONE_FOURTH_HALF + ONE_FOURTH_HALF_HALF);  // 1`_ 1`-_
+    WRITE_NOTE(D_la, ONE_FOURTH_HALF_HALF);                                          // 6__
+    WRITE_NOTE(D_re * 2, ONE_FOURTH_HALF + ONE_FOURTH_HALF + ONE_FOURTH_HALF);       // 2`_ 2`_ 2`_
+    WRITE_NOTE(D_do * 2, ONE_FOURTH_HALF);                                           // 1`_
 
     // 百花盛
     // 2/4 6 5_ 1`_
